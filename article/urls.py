0a740ceed8fa156abf1fffaf656fa1ae1dd4ea15
Nitish Kumar <nitish@pushowl.com>
2023-04-24 04:42:50 +0530

    fix: url with appended /

--- article/urls.py
+++ article/urls.py
@@ -4,5 +4,5 @@ from .views.article import ArticleView
 
 urlpatterns = [
     path('', ArticleView.as_view(), name='article_create_list_view'),
-    path('<int:article_id>/', ArticleView.as_view(), name='article_detail_update_delete_view'),
+    path('<int:article_id>', ArticleView.as_view(), name='article_detail_update_delete_view'),
 ]
